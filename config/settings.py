4f3b698c83f2212599899a3dbf0f744ad06cb726
iampsih <merei.oralbek@gmail.com>
2026-02-27 10:30:57 +0500

    sql changed, new migrations, html changed to figma style

--- config/settings.py
+++ config/settings.py
@@ -80,9 +80,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "test_task",
+        "USER": "django",
+        "PASSWORD": "django123",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {"charset": "utf8mb4"},
     }
 }
 
